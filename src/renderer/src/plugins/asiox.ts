7d1f64939d9b0e4a297a6184568976b196de9c2a
yy <691335336@qq.com>
2023-07-29 14:41:35 +0800

    [IMP] 修改请求地址

--- src/renderer/src/plugins/asiox.ts
+++ src/renderer/src/plugins/asiox.ts
@@ -2,7 +2,7 @@
  * @Author: yy 691335336@qq.com
  * @Date: 2023-07-09 18:11:47
  * @LastEditors: yy 691335336@qq.com
- * @LastEditTime: 2023-07-16 16:46:39
+ * @LastEditTime: 2023-07-24 12:55:27
  * @FilePath: /wallpaper/src/renderer/src/plugins/asiox.ts
  * @Description: 封装axios
  */
@@ -13,7 +13,8 @@ let loadingService
 
 const request = axios.create({
   // baseURL: import.meta.env.DEV ? 'http://localhost:3000' : 'http://81.68.193.193:3000',
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'http://47.98.112.36:7000',
   timeout: 10000
 })
 
